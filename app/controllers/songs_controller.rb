cbcd3b32cc78a1c32615928d2e40a43dc5835f87
github username <github email address>
2019-12-31 11:29:59 -0500

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -9,11 +9,12 @@ class SongsController < ApplicationController
 
   def new
     @song = Song.new
+
+    3.times { @song.notes.build }
   end
 
   def create
     @song = Song.new(song_params)
-
     if @song.save
       redirect_to @song
     else
@@ -47,7 +48,60 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title)
+    params.require(:song).permit(:title, :genre_id, :artist_name, notes_attributes: [:content])
   end
 end
+# class SongsController < ApplicationController
+#   def index
+#     @songs = Song.all
+#   end
+
+#   def show
+#     @song = Song.find(params[:id])
+#   end
+
+#   def new
+#     @song = Song.new
+#     @song.notes.build
+#   end
+
+#   def create
+#     @song = Song.new(song_params)
+
+#     if @song.save
+#       redirect_to @song
+#     else
+#       render :new
+#     end
+#   end
+
+#   def edit
+#     @song = Song.find(params[:id])
+#   end
+
+#   def update
+#     @song = Song.find(params[:id])
+
+#     @song.update(song_params)
+
+#     if @song.save
+#       redirect_to @song
+#     else
+#       render :edit
+#     end
+#   end
+
+#   def destroy
+#     @song = Song.find(params[:id])
+#     @song.destroy
+#     flash[:notice] = "Song deleted."
+#     redirect_to songs_path
+#   end
+
+#   private
+
+#   def song_params
+#     params.require(:song).permit(:title, :genre_id, :artist_name, notes_attributes: [:content])
+#   end
+# end
 
